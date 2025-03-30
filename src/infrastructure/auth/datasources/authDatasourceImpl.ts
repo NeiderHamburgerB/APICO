e90f740c7f5268c9edea9ecfd59d655d1539626f
neiderhamburgerImaginamos <neider.hamburger@imaginamos.com>
2025-03-30 10:58:16 -0500

    feat: infrastructures auth - add services

---
+++ src/infrastructure/auth/datasources/authDatasourceImpl.ts
@@ -0,0 +1,5 @@
+import { AuthRepository } from '../../../domain/auth/repositories/auth.repository';
+import { injectable } from 'tsyringe';
+
+@injectable()
+export class AuthDatasourceImpl implements AuthRepository {}
